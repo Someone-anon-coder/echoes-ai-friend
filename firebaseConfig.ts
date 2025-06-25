d71c7f5548f5074e038cb3e0eaf1cb4a261fae98
Someone <aayush.c2684@gmail.com>
2025-06-25 19:21:51 +0530

    chore: remove Firebase configuration files from .gitignore and delete firebaseConfig.ts

--- firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyASeFGh4piroxG0murUx01eus8haQbEdjs",
-  authDomain: "echoesai-66cb4.firebaseapp.com",
-  projectId: "echoesai-66cb4",
-  storageBucket: "echoesai-66cb4.firebasestorage.app",
-  messagingSenderId: "160638372769",
-  appId: "1:160638372769:web:956447436a762f44ff99e0",
-  measurementId: "G-9TD2EV0TKD"
-};
