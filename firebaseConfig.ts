791730e5c15fbd4033f954dd96080d373fccffe3
Someone <136891153+Someone-anon-coder@users.noreply.github.com>
2026-02-14 16:11:19 +0530

    Replace Firebase config values with placeholders
    Updated Firebase configuration to use placeholders for sensitive information.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZs5BrsALiTRWi2ON7S_ID7BXDqTTzno8",
-  authDomain: "aura-wellness-hackathon-472215.firebaseapp.com",
-  projectId: "aura-wellness-hackathon-472215",
-  storageBucket: "aura-wellness-hackathon-472215.firebasestorage.app",
-  messagingSenderId: "926187786081",
-  appId: "1:926187786081:web:20a0fb47f1032dc9113f20",
+  apiKey: [API_KEY],
+  authDomain: [AUTH_DOMAIN],
+  projectId: [PROJECT_ID],
+  storageBucket: [STORAGE_BUCKET],
+  messagingSenderId: [MESSAGE_SENDER_ID],
+  appId: [APP_ID],
   // Make sure to add your OAuth Client ID here if you are using Google Sign-In
   // clientId: "YOUR_OAUTH_CLIENT_ID.apps.googleusercontent.com",
 };
